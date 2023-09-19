abe3b59aaf1cacd0c38183f476ec0871210a363e
LSHHQ <138203376+LSHHQ@users.noreply.github.com>
2023-09-19 09:53:46 +0900

    [Gold V] Title: 최소비용 구하기, Time: 360 ms, Memory: 46132 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,61 +1,83 @@
-import java.io.*;
 import java.util.*;
-
+import java.io.*;
+	
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-
-	public static void main(String[] args) throws Exception {
-
+	static int[][] cost; 
+	static int N;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st;
 		
-		int N = Integer.parseInt(br.readLine()); //정점의 수
-		int M = Integer.parseInt(br.readLine()); // 간선의 수
+		N = Integer.parseInt(br.readLine()); //도시의 갯수 
+		int M = Integer.parseInt(br.readLine()); //버스의 갯수 
 		
-		List<ArrayList<int[]>> list = new ArrayList<>();
-		int[] DP = new int[N+1];
-		
-		for (int i = 0; i <= N; i++) {
-			list.add(new ArrayList<int[]>());
-			Arrays.fill(DP, (int)Integer.MAX_VALUE);
+		cost = new int[N][N]; 
+		for(int i=0; i<N; i++) {
+			Arrays.fill(cost[i], -1); 
 		}
 		
-		
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			int cost = Integer.parseInt(st.nextToken());
+		while(M-- >0) {
+			st= new StringTokenizer(br.readLine(), " ");
+			int a= Integer.parseInt(st.nextToken())-1;
+			int b= Integer.parseInt(st.nextToken())-1;
+			int c= Integer.parseInt(st.nextToken());
 			
-			list.get(from).add(new int[] {to, cost});
+			if(cost[a][b] != -1) {
+				cost[a][b] = Math.min(cost[a][b], c); 
+			}
+			else cost[a][b] = c;
+			
 		}
 		
-		st = new StringTokenizer(br.readLine());
-		int start = Integer.parseInt(st.nextToken());
-		int end = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine(), " ");
+		int start= Integer.parseInt(st.nextToken())-1;
+		int end = Integer.parseInt(st.nextToken())-1; 
 		
+		int res= dijkstra(start, end); 
+		System.out.println(res);
+
+	}
+	static class Node implements Comparable<Node>{
+		int city;
+		int dis;
 		
-		PriorityQueue<int[]> PQ = new PriorityQueue<>((a,b)-> {return a[1] - b[1] ;});
+		public Node(int city, int dis) {
+			this.city=city;
+			this.dis=dis;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			return this.dis- o.dis;
+		}
+	}
+	static int dijkstra(int start, int end) {
+		int[] min= new int[N]; //출발지에서 각 도시까지의 최소비용저장
+		Arrays.fill(min, Integer.MAX_VALUE) ;
 		
-		PQ.add(new int[] {start, 0});
-		DP[start] = 0;
-		while(!PQ.isEmpty()) {
-			int from = PQ.poll()[0];
+		PriorityQueue<Node> que= new PriorityQueue<>();
+		que.add(new Node(start,0)); 
+		
+		while(!que.isEmpty()) {
+			Node n = que.poll();
+			int city = n.city;
+			int dis = n.dis;
 			
-			if(from == end)
-				break;
-			
-			for(int[] node : list.get(from)) {
-				int to = node[0];
-				int cost = node[1];
-				
-				if(DP[from] + cost < DP[to]) {
-					DP[to] = DP[from] + cost;
-					PQ.add(new int[] {to, DP[to]});
+			for(int i=0; i<N; i++) {
+				if(cost[city][i] != -1) {
+					if(min[i] > cost[city][i] + dis) {
+						min[i] = cost[city][i] +dis; 
+						que.add(new Node(i, min[i])); 
+					}
 				}
 			}
+			
 		}
+		return min[end]; 
+//		for(int i=0; i<N; i++) {
+//			System.out.print(min[i]+" ");
+//		}
+//		System.out.println();
 		
-		System.out.println(DP[end]);
 	}
-
-}// class
+}//end of class 
