4d0eb454c737aac2e8a41754001eee652025f444
LSHHQ <138203376+LSHHQ@users.noreply.github.com>
2023-10-12 10:37:18 +0900

    [Gold V] Title: 톱니바퀴, Time: 80 ms, Memory: 11924 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
@@ -20,16 +20,16 @@ public class Main {
 		for (int i = 0; i < 4; i++) {
 			char[] NS = br.readLine().toCharArray();
 			for (int j = 0; j < 8; j++) {
-				list.get(i).addLast(NS[j] - '0'); // 0: 12시 ... 2:3시(오른쪽 인접)....6: 9시(왼쪽 인접)
+				list.get(i).addLast(NS[j] - '0');
 			}
 		}
 
-		int N = Integer.parseInt(br.readLine()); // 바퀴 회전 수
+		int N = Integer.parseInt(br.readLine());
 		for (int i = 0; i < N; i++) {
 
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int cogNum = Integer.parseInt(st.nextToken()) - 1; // 인덱스에 맞추기 위해 -1
-			int dir = Integer.parseInt(st.nextToken()); // 1: 시계., -1:반시계
+			int cogNum = Integer.parseInt(st.nextToken()) - 1; 
+			int dir = Integer.parseInt(st.nextToken());
 			visited = new boolean[4];
 			revolve(cogNum, dir);
 		}
@@ -38,8 +38,6 @@ public class Main {
 		for (int i = 0; i < 4; i++)
 			sum += list.get(i).get(0) * Math.pow(2, i);
 
-	
-
 		System.out.println(sum);
 
 	}
@@ -47,12 +45,12 @@ public class Main {
 	static List<LinkedList<Integer>> list = new ArrayList<>();
 	static boolean[] visited = new boolean[4];
 
-	static int revolve(int cogNum, int dir) { // 이걸로 재귀식으로 옆에있는애 돌려버리고, 그애도 다른애 돌릴 수 있는데 옆에 애 돌렸을때 다시 돌아와서 우리 애 돌릴까봐 ..
+	static void revolve(int cogNum, int dir) {
 
 		if (visited[cogNum])
-			return 0;
+			return;
 
-		visited[cogNum] = true; // 다시 돌아와서 돌리는거 방지
+		visited[cogNum] = true;
 
 		int myRight = list.get(cogNum).get(2);
 		int myLeft = list.get(cogNum).get(6);
@@ -63,30 +61,18 @@ public class Main {
 		if (cogNum + 1 < 4)
 			yourLeft = list.get(cogNum + 1).get(6);
 
-//		System.out.print(cogNum+1+"   ");
-//		for (int i = 0; i < 8; i++) {
-//			System.out.print(list.get(cogNum).get(i)+" ");
-//		}
-//		System.out.println();
-
 		if (dir == 1)
 			list.get(cogNum).addFirst(list.get(cogNum).pollLast());
 		else if (dir == -1)
 			list.get(cogNum).addLast(list.get(cogNum).pollFirst());
 
-//		System.out.print(cogNum+1+"   ");
-//		for (int i = 0; i < 8; i++) {
-//			System.out.print(list.get(cogNum).get(i)+" ");
-//		}
-//		System.out.println();
-
 		if (myRight != yourLeft)
-			revolve(cogNum + 1, -dir); //여기서 한쪽만 돌려버리고 끝나버림, 양쪽 다 돌릴수도 있는데..
+			revolve(cogNum + 1, -dir);
 
 		if (myLeft != yourRight)
 			revolve(cogNum - 1, -dir);
 
-		return 0;
+		return;
 	}
 
 }
