815007194ff98c61a73f472d576f3d07537fa1fe
LSHHQ <138203376+LSHHQ@users.noreply.github.com>
2023-10-25 11:57:26 +0900

    [Gold I] Title: 부분수열의 합 2, Time: 760 ms, Memory: 102672 KB -BaekjoonHub

--- 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.java
+++ 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.java
@@ -1,79 +1,61 @@
 import java.io.*;
 import java.util.*;
+import java.util.function.Function;
+
+// 좋은 풀이 저장
+// 이분탐색 안하고 한쪽 배열에서 나올 수 있는 합을 구해놓고
+// 다른쪽 배열에서 나올 수 있는 합과 조합했을 때 S 가 나오면 카운트하는 방식
 
 public class Main {
-	
-	static int stoi(String s) {return Integer.parseInt(s);}
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st ;
-		
-		st = new StringTokenizer(br.readLine());
-		N = stoi(st.nextToken());
-		S = stoi(st.nextToken());
-		
-		arr = new int[N];
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) arr[i]  = stoi(st.nextToken());
-		
-		// 배열 둘로 나누고, 조합을 통해 나올 수 있는 모든 수열의 합 경우의 수를 저장한다
-		// 정렬한다음 투포인터로 답을 구한다
-		left = new ArrayList<Integer>();
-		right = new ArrayList<Integer>();
-		
-		makeSumCase(0, 0, N/2, left);
-		makeSumCase(0, N/2, N, right);
-		Collections.sort(left);
-		Collections.sort(right);
-		
-		int lenL = left.size();
-		int lenR = right.size();
-		int L = 0;
-		int R = lenR-1;
-		long cnt = 0;
-		while(true) {
-			if(L==lenL || R ==-1) break;
-			int valL = left.get(L);
-			int valR = right.get(R);
-			
-			if(valL+valR==S) {
+	static Function<String, Integer> stoi = Integer::parseInt;
+	static int n, m;
+	static long res;
+	static int[] nums;
+	static Map<Integer, Integer> map;
 
-				long cntL = 1;
-				while(L<lenL-1 && left.get(L+1)==valL) {
-					cntL++;
-					L++;
-				}
-				
-				long cntR = 1;
-				while(R>0 && right.get(R-1)==valR) {
-					cntR++;
-					R--;
-				}
-				cnt += cntL*cntR;
-				L++;
-				R--;
-			}
-			else if(valL+valR<S) L++;
-			else if(valL+valR>S) R--;
-		}
-		// 두 리스트 모두 아무것도 선택 안했을때로, 0인 경우를 가지고 있기 때문에 하나의 리스트에서 S가 나오는 경우도 반영이 된다.
-		// S==0인 경우를 찾을 경우에는, 두 리스트 모두 아무것도 선택 안했을 때 의 경우가 하나 추가돼서 카운트 되니까 -1 해줘야된다.
-		System.out.println(S==0? --cnt : cnt);
+	public static void main(String[] args) throws IOException {
+		sol();
 	}
-	
-	static int N, S;
-	static int[] arr;
-	static List<Integer> left, right;
-	
-	static void makeSumCase(int sum, int idx, int end, List<Integer> list) {
-		if(idx==end) {
-			list.add(sum);
+
+	private static void sol() throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+
+		n = stoi.apply(st.nextToken());
+		m = stoi.apply(st.nextToken());
+
+		nums = new int[n];
+		st = new StringTokenizer(in.readLine());
+		for (int i = 0; i < n; i++) {
+			nums[i] = stoi.apply(st.nextToken());
+		}
+
+		map = new HashMap<>();
+		res = 0;
+		right(n / 2, 0);
+		left(0, 0);
+
+		System.out.println(m == 0 ? res - 1 : res);
+	}
+
+	private static void left(int idx, int sum) {
+		if (idx == n / 2) {
+			if(map.get(m - sum) != null) {
+			res += map.get(m - sum);
+			}
 			return;
 		}
-		makeSumCase(sum, idx+1, end, list); // 현재 인덱스 선택하지 않는 경우의 수
-		makeSumCase(sum+arr[idx], idx+1, end, list); //현재 인덱스 선택해 집합에 포함하는 경우
+		left(idx + 1, sum);
+		left(idx + 1, sum + nums[idx]);
 	}
-	
-	
-}// Main
+
+	private static void right(int idx, int sum) {
+		if (idx == n) {
+			map.put(sum, map.getOrDefault(sum, 0) + 1);
+			return;
+
+		}
+		right(idx + 1, sum);
+		right(idx + 1, sum + nums[idx]);
+	}
+}
