70d4571db28affbc568003e51a392dfa407344cc
LSHHQ <138203376+LSHHQ@users.noreply.github.com>
2023-10-30 14:07:34 +0900

    [Gold III] Title: 세 용액, Time: 172 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.java
+++ 백준/Gold/2473. 세 용액/세 용액.java
@@ -25,54 +25,38 @@ public class Main {
 		for (int i = 0; i < N; i++) arr[i] = stoi(st.nextToken());
 		Arrays.sort(arr);
 		
-		int[] idx = new int[N];
+		int[] idx = new int[3];
 		long min = Long.MAX_VALUE;
-		outter: for (int i = 0; i <= N-3; i++) {
-			for (int j = i+1; j <= N-2; j++) {
+		int F = -1; //first index
+		int L = 0, R = N-1;
+		
+		outter:while(F++ < N-3) {
+			L = F+1;
+			R = N-1;
+			
+			//투 포인터
+			while(L!=R) {
 				
-				//이분탐색 돌리기
-				int left = j+1;
-				int right = N-1;
-				int mid = 0;
-				long sum = 0;
+				long sum = (long)arr[F]+arr[L]+arr[R];
 				
-				while(left<=right) {
-					
-					mid = (left+right)>>1;
-				
-					//반복문의 종료가 인풋에따라 다르게 이루어져서
-					//종료시점이 최소일때 임을 보장하지 않는다.
-					//내 생각엔 종료시점 mid +-1 이 최소일것같다. >>아닌걸로 확인했음.
-					// 최소가 되는 시점이 마지막 부근이 아님.
-					// 중간 시점에 있는 경우가 많이 있음.
-					// 아래 구문이 반복문 안에 있는것은 필수적이다.
-					sum = (long)arr[i]+arr[j]+arr[mid];
-					long tmp = Math.abs(sum);
-					if(tmp<min) {
-						min = tmp;
-						idx = new int[] {i,j,mid};
-					}
-				
-					
-					if(sum<0) left = mid+1;
-					else if(sum>0) right = mid-1;
-					else if(sum==0) {
-						idx = new int[] {i,j,mid};
-						break outter;
-					}
+				if(Math.abs(sum)<min) {
+					min = Math.abs(sum);
+					idx = new int[] {F, L, R};
 				}
 				
-//				mid = (left+right)>>1;
-//				sum = (long)arr[i]+arr[j]+arr[mid];
-//				long tmp = Math.abs(sum);
-//				if(tmp<min) {
-//					min = tmp;
-//					idx = new int[] {i,j,mid};
-//				}
-			} 
+				if(sum<0) L++;
+				else if(sum>0) R--;
+				else {
+					idx = new int[] {F, L, R};
+					break outter;
+				}
+				
+			}
 		}
 		
 		System.out.println(arr[idx[0]]+" "+arr[idx[1]]+" "+arr[idx[2]]);
+		
+		
 		
 	}
 }
