4241933f3e87b1899f337c9a5500c07fb004cdf0
LSHHQ <138203376+LSHHQ@users.noreply.github.com>
2023-11-02 13:42:58 +0900

    Update 최솟값 찾기.java

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -21,7 +21,7 @@ public class Main {
 			int[] tmp = new int[] {stoi(st.nextToken()),i}; //밸류, 인덱스 
 			// 범위 밖인 밸류 버리기
 			while(!deq.isEmpty() && deq.peekFirst()[1] <= i-M) deq.pollFirst();
-			// 
+			// 지금 들어오는 값보다 큰 값은 어차피 들어온 값이 나갈때까지 최솟값으로 출력되지 않으니까 필요없으니까 버린다.
 			while(!deq.isEmpty() && deq.peekLast()[0] > tmp[0]) deq.pollLast();
 			deq.addLast(tmp);
 			bw.write(String.valueOf(deq.peekFirst()[0])+" ");
