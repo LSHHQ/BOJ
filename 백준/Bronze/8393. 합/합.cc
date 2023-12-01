1265b3cfdd8c81456f1db3a2a9db254f2dea0000
LSHHQ <138203376+LSHHQ@users.noreply.github.com>
2023-12-02 01:31:13 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main()
+{
+		ios::sync_with_stdio(0);
+		cin.tie(0);
+		cout.tie(0);
+            int N;
+            cin >> N;
+          
+        cout << N*(N+1)/2;
+
+}
